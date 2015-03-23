a158964c649b9bb685d3c50d63a9842d240c54fb
James Hall <james_hall@live.ca>
2015-03-23 11:37:43 -0400

    Moved dashboard assets out of binary

--- dashboard/handle_assets.go
+++ dashboard/handle_assets.go
@@ -3,22 +3,28 @@ package dashboard
 import (
 	"github.com/BruteForceFencer/core/config"
 	"github.com/BruteForceFencer/core/hitcounter"
+	"html/template"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 )
+
+// installPath is the path to the BFF installation in slash form.
+var installPath string
+
+func init() {
+	determineInstallPath()
+}
 
 // HandleAssets serves the HTML, CSS and JS assets for the dashboard.
 func (s *Server) HandleAssets(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/core.css":
-		w.Header().Set("Content-Type", "text/css")
-		w.Write([]byte(coreCss))
-	case "/core.js":
-		w.Header().Set("Content-Type", "application/js")
-		w.Write([]byte(coreJs))
-	case "/":
+	if r.URL.Path == "/" {
 		s.serveHomePage(w, r)
-	default:
-		http.NotFound(w, r)
+	} else {
+		assetPath := path.Join("assets", r.URL.Path)
+		assetPath = filepath.FromSlash(fromInstallPath(assetPath))
+		http.ServeFile(w, r, assetPath)
 	}
 }
 
@@ -35,5 +41,36 @@ func (s *Server) serveHomePage(w http.ResponseWriter, r *http.Request) {
 		Directions:    s.conf.Directions,
 	}
 
-	s.t.Execute(w, data)
+	htmlPath := filepath.FromSlash(fromInstallPath("assets/dashboard.html"))
+	t, err := template.ParseFiles(htmlPath)
+	if err != nil {
+		http.Error(w, "Unable to find server files.", http.StatusInternalServerError)
+		return
+	}
+
+	t.Execute(w, data)
+}
+
+// fromInstallPath returns the absolute path by appending p to the installation
+// path.  The result is in slash form.
+func fromInstallPath(p string) string {
+	return path.Join(installPath, p)
+}
+
+// determineInstallPath sets the installPath variable.
+func determineInstallPath() {
+	// The path to the executable binary.
+	var execPath string
+
+	// Argument 0 converted to slash notation.
+	arg0 := filepath.ToSlash(path.Clean(os.Args[0]))
+	if len(arg0) > 0 && arg0[0] == '/' {
+		// The 0th arg is the absolute path.
+		execPath = path.Clean(arg0)
+	} else {
+		wd, _ := os.Getwd()
+		execPath = path.Join(wd, arg0)
+	}
+
+	installPath = path.Dir(path.Dir(execPath))
 }
--- dashboard/server.go
+++ dashboard/server.go
@@ -4,7 +4,6 @@ package dashboard
 import (
 	"github.com/BruteForceFencer/core/config"
 	"github.com/BruteForceFencer/core/hitcounter"
-	"html/template"
 	"net/http"
 )
 
@@ -14,7 +13,6 @@ type Server struct {
 	mux     *http.ServeMux
 	conf    *config.Configuration
 	counter *hitcounter.HitCounter
-	t       *template.Template
 }
 
 // New returns an initialized instance of *Server.
@@ -23,7 +21,6 @@ func New(conf *config.Configuration, counter *hitcounter.HitCounter) *Server {
 	result.conf = conf
 	result.counter = counter
 	result.mux = http.NewServeMux()
-	result.t = template.Must(template.New("core").Parse(coreHtml))
 	result.Server = http.Server{
 		Addr:    conf.DashboardAddress,
 		Handler: result.mux,
